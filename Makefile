8beb264368fabd42111f0e3e5aa3e508da9b7387
Paul Schnisa <paul.schnisa@proton.me>
2024-01-11 21:23:13 +0100

    geändert:       Makefile 	gelöscht:       src/donut.c 	geändert:       src/main.cpp

--- Makefile
+++ Makefile
@@ -1,4 +1,2 @@
 main: src/main.cpp
 	g++ -o ced src/main.cpp
-donut: src/donut.c
-	gcc -o donut src/donut.c -lm
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
-#include <stdio.h>
 #include <string.h>
+#include <iostream>
 #include <ncurses.h>
 // Author: Paul-Jonas Schnisa
-// License: CC0
+// License: GNU GPL-3.0
 // https://github.com/scuidthesquid/ced/
+
+
+
+int main(int argc, char *argv[]) {
+    
+    return 0;
+}
